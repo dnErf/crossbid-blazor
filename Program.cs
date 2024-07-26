96a52744d113d962aa954f55a3a52045295a8ec6
ff <frend.fernando@LIVE.COM>
2024-07-26 09:13:30 -0700

    up remove launchSettings | supabase + dapper | sample nav head

--- Program.cs
+++ Program.cs
@@ -4,6 +4,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
+var url = Environment.GetEnvironmentVariable("SUPABASE_URL");
+var key = Environment.GetEnvironmentVariable("SUPABASE_KEY");
+
+var options = new Supabase.SupabaseOptions
+{
+	AutoRefreshToken = true,
+	AutoConnectRealtime = true,
+	// SessionHandler = new SupabaseSessionHandler() <-- This must be implemented by the developer
+};
+
+// Note the creation as a singleton.
+builder.Services.AddTransient(provider => new Supabase.Client(url!, key, options));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
